78d5288af5d50d7f0211e0078f51052a9fa578be
Francisco Rodriguez <fjrodriguez.353@gmail.com>
2024-05-15 22:40:55 -0300

    feat: add react navigation

--- App.js
+++ App.js
@@ -1,12 +1,32 @@
 import { StatusBar } from 'expo-status-bar'
 import { SafeAreaProvider } from 'react-native-safe-area-context'
-import { Welcome } from './screens/welcome'
+import { useFonts } from 'expo-font'
+import * as SplashScreen from 'expo-splash-screen'
+import { useEffect } from 'react'
+import { Navigator } from './navigation/navigator'
+
+SplashScreen.preventAutoHideAsync()
 
 export default function App() {
+  const [fontsLoaded, fontError] = useFonts({
+    'Unbounded-Bold': require('./assets/fonts/Unbounded-Bold.ttf'),
+    'Unbounded-Regular': require('./assets/fonts/Unbounded-Regular.ttf'),
+  })
+
+  useEffect(() => {
+    const onLayoutRootView = async () => {
+      if (fontsLoaded || fontError) {
+        await SplashScreen.hideAsync()
+      }
+    }
+    onLayoutRootView()
+  }, [fontsLoaded, fontError])
+
+  if (!fontsLoaded && !fontError) return null
   return (
     <SafeAreaProvider>
       <StatusBar backgroundColor='white' style='dark' />
-      <Welcome />
+      <Navigator />
     </SafeAreaProvider>
   )
 }
---
+++ navigation/routes.js
@@ -0,0 +1,6 @@
+export const ROUTE = {
+  WELCOME: 'Welcome',
+  HOME: 'Home',
+  ITEM_LIST_CATEGORIES: 'ItemListCategories',
+  ITEM_DETAIL: 'ItemDetail',
+}
