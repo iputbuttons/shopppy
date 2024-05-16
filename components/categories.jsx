c5e1890aa308c3d7eced3d8ef4dbe5240748d40b
Francisco Rodriguez <fjrodriguez.353@gmail.com>
2024-05-15 22:42:28 -0300

    feat: add components

--- components/categories.jsx
+++ components/categories.jsx
@@ -1,18 +1,33 @@
 import { FlatList, StyleSheet, Text, View } from 'react-native'
 import data from '../data/categories.json'
 import { CategoryItem } from './categoryItem'
+import { useNavigation } from '@react-navigation/native'
+import { ROUTE } from '../navigation/routes'
 
-export const Categories = () => (
-  <View style={styles.categories}>
-    <Text style={styles.text}>Marcas top</Text>
-    <FlatList
-      contentContainerStyle={styles.list}
-      data={data}
-      horizontal
-      renderItem={({ item }) => <CategoryItem name={item} />}
-    />
-  </View>
-)
+export const Categories = () => {
+  const { navigate } = useNavigation()
+
+  return (
+    <View style={styles.categories}>
+      <Text style={styles.text}>Marcas top</Text>
+      <FlatList
+        contentContainerStyle={styles.list}
+        data={data}
+        horizontal
+        renderItem={({ item }) => (
+          <CategoryItem
+            name={item}
+            onPress={() =>
+              navigate(ROUTE.ITEM_LIST_CATEGORIES, {
+                brand: item,
+              })
+            }
+          />
+        )}
+      />
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
   categories: {
