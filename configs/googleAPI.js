19cbbc287fc82dd61fcfdc4800e6503c0620cf29
Francisco Rodriguez <fjrodriguez.353@gmail.com>
2024-06-10 12:43:50 -0300

    keys

--- configs/googleAPI.js
+++ configs/googleAPI.js
@@ -1,4 +1,4 @@
 export const googleAPI = {
-  mapStatic: 'AIzaSyCHlscJUHVWLxZ1w2QwoNYReXBf-9lWTBk',
-  geocoding: 'AIzaSyAObqiIJmgc3gZA8GjItd9F_DC89Ro5IsY',
+  mapStatic: 'KEY',
+  geocoding: 'KEY',
 }
